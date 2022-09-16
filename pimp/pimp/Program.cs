25e6c3200329d59028e4f0274d0f428bccf70386
hedenjacob <43139424+hedenjacob@users.noreply.github.com>
2022-09-16 09:50:07 +0200

    Add files via upload

---
+++ pimp/pimp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pimp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor myColor = ConsoleColor.Green;
+            Console.ForegroundColor = myColor;
+            Console.WriteLine("The foreground color is {0}.", myColor);
+            Console.Beep();
+            Console.Title = "DJ MOKAI";           
+        }
+    }
+}
